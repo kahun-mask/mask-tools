1639f74ba2e7b4e90e94ca8cba76e212bff141bb
kahun-mask <kahun-mask@users.noreply.github.com>
2018-12-26 23:52:14 +0900

    remove files

--- packages/web-authn/__tests__/web-authn.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const webAuthn = require('..');
-
-describe('web-authn', () => {
-    it('needs tests');
-});
--- packages/web-authn/lib/web-authn.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WebAuthn = /** @class */ (function () {
-    function WebAuthn() {
-    }
-    return WebAuthn;
-}());
-exports.WebAuthn = WebAuthn;
-//# sourceMappingURL=web-authn.js.map
